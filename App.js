40dae579e4707b64006bbf912b21123b99e73a67
RAHUL TANWAR <rahul@magicapps.co.in>
2021-12-27 16:18:53 +0530

    FlastList

--- App.js
+++ App.js
@@ -1,12 +1,76 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { FlatList, StyleSheet, Text, View } from 'react-native';
+import { fakeServer } from './fakeServer';
+
+const renderItem = ({ item }) => {
+  return (
+    <Text
+      style={{
+        textAlign: 'center',
+        fontWeight: 'bold',
+        fontSize: 20,
+        padding: 15,
+        borderBottomColor: 'red',
+        borderBottomWidth: 2,
+      }}
+    >
+      {item}
+    </Text>
+  );
+};
+
+let stopFetchMore = true;
+
+const ListFooterComponent = () => (
+  <Text
+    style={{
+      fontSize: 16,
+      fontWeight: 'bold',
+      textAlign: 'center',
+      padding: 5,
+    }}
+  >
+    Loading...
+  </Text>
+);
 
 export default function App() {
+  const [data, setData] = useState([]);
+  const [loadingMore, setLoadingMore] = useState(false);
+
+  const fetchData = async () => {
+    const response = await fakeServer(20);
+    setData([...response]);
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const handleOnEndReached = async () => {
+    setLoadingMore(true);
+    if (!stopFetchMore) {
+      const response = await fakeServer(20);
+      if (response === 'done') return setLoadingMore(false);
+      setData([...data, ...response]);
+      stopFetchMore = true;
+    }
+    setLoadingMore(false);
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <FlatList
+      data={data}
+      keyExtractor={item => item}
+      renderItem={renderItem}
+      onEndReached={handleOnEndReached}
+      onEndReachedThreshold={0.5}
+      onScrollBeginDrag={() => {
+        stopFetchMore = false;
+      }}
+      ListFooterComponent={() => loadingMore && <ListFooterComponent />}
+    />
   );
 }
 
